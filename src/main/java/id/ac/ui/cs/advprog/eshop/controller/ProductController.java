2964a4c709d356ab70304c2a40552a19e6e19cca
jeanxwang <fazaqinthara629@gmail.com>
2024-02-14 18:56:35 +0700

    adjust case sensitivity in controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -21,7 +21,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -34,14 +34,14 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
     @GetMapping("/edit/{id}")
     public String editProductPage(@PathVariable("id") String id, Model model) {
         Product product = service.findById(id);
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PostMapping("/edit")

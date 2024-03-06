548930ee7eb6517633f29481badcd8df94104a77
jeanxwang <fazaqinthara629@gmail.com>
2024-03-06 19:28:15 +0700

    [RED] Add CreatePayment method test

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -17,4 +17,18 @@ class PaymentTest {
         paymentData.put("bankName", "Example Bank");
         paymentData.put("referenceCode", "123456789");
     }
+
+    @Test
+    void testCreatePayment() {
+        Payment payment = new Payment();
+        payment.setId("123456");
+        payment.setMethod("Bank Transfer");
+        payment.setStatus("WAITING");
+        payment.setPaymentData(paymentData);
+
+        assertEquals("123456", payment.getId());
+        assertEquals("Bank Transfer", payment.getMethod());
+        assertEquals("WAITING", payment.getStatus());
+        assertEquals(paymentData, payment.getPaymentData());
+    }
 }

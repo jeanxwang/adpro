823e0fa6bda00f3ab81466a3458cf8c0e0270cdb
jeanxwang <fazaqinthara629@gmail.com>
2024-03-06 19:26:33 +0700

    [RED] Add PaymentTest setup

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -10,4 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PaymentTest {
     private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        this.paymentData = new HashMap<>();
+        paymentData.put("bankName", "Example Bank");
+        paymentData.put("referenceCode", "123456789");
+    }
 }
